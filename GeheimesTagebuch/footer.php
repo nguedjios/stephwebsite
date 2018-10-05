acab8860e6124d5d912df27437533868bb6e224a
snankam <42391882+snankam@users.noreply.github.com>
2018-10-05 12:29:09 +0200

    Add files via upload

---
+++ GeheimesTagebuch/footer.php
@@ -0,0 +1,34 @@
+<script type="text/javascript">
+      
+            $(".toggelForms").click(function(){
+                
+               $("#signUpForm").toggle();
+                $("#loginForm").toggle();
+                
+            });
+        
+    
+        $('#diary').bind('input propertychange', function() {
+            
+            $.ajax({
+                method: "POST",
+              url: "updateDatabase.php",
+              data: { content: $("#diary").val()}
+            }).done(function(msg){
+                
+               alert("Daten gespeichert: " + msg); 
+                
+            });
+                
+                
+
+        });
+      
+      
+      </script>
+      
+      
+      
+
+  </body>
+</html>
