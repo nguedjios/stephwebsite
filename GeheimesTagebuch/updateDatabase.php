240e0d77a83e9f7bcba7af9ca2b5766178d59d8c
snankam <42391882+snankam@users.noreply.github.com>
2018-10-05 12:17:39 +0200

    Add files via upload

---
+++ GeheimesTagebuch/updateDatabase.php
@@ -0,0 +1,28 @@
+<?php
+    
+    session_start();
+
+    if(array_key_exists("content", $_POST)){
+        
+        include("connection.php");
+        
+        $query = "UPDATE `users` SET `diary` = '".mysqli_real_escape_string($link, $_POST['content'])."' WHERE id = ".mysqli_real_escape_string($link, $_SESSION['id'])." LIMIT 1";
+        
+        if(mysqli_query($link, $query)){
+
+            
+        }else{
+            
+            echo "Versenden an die Datenbank ist schiefgegangen. Melde dich beim Admin.";
+            
+        }
+        
+        
+        
+    }
+
+
+
+
+
+?>
