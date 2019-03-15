d307206a395aca44915c5cd6ac62f2a65702f001
snankam <42391882+snankam@users.noreply.github.com>
2019-03-15 12:37:40 +0100

    Add files via upload

---
+++ GeheimesTagebuch php/connection.php
@@ -0,0 +1,11 @@
+<?php
+
+$link = mysqli_connect("localhost", "u390772130_ewuly", "D=XW?O~`lF34", "u390772130_ejygy");
+        
+        if(mysqli_connect_error()){
+            
+            die("Verbindung zur Datenbank konnte nicht aufgebaut werden.");
+            
+        }
+
+?>
