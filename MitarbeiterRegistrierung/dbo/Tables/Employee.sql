fac326e4c5ae92c3cfe6183578ed335e8e4f669a
snankam <42391882+snankam@users.noreply.github.com>
2019-03-15 12:40:29 +0100

    Delete Employee.sql

--- MitarbeiterRegistrierung/dbo/Tables/Employee.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[Employee] (
-    [Id]         INT            IDENTITY (1, 1) NOT NULL,
-    [Name]        NVARCHAR (MAX) NULL,
-    [Vorname]     NVARCHAR (MAX) NULL,
-    [Jobtitel]    NVARCHAR (MAX)  NULL, 
-    [Abteilung]   NVARCHAR (MAX)  NULL,
-    [Foto]        BINARY (MAX)  NULL, 
-    CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
